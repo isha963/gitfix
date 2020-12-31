1f309b206fdb7d76b3598daf8c1a8dc429bba832
isha7337 <isha.bisht.2781@gmail.com>
2020-12-31 15:59:00 +0530

    first code

---
+++ Untitled1.c
@@ -0,0 +1,23 @@
+/* code for arithmetic operators
+*/
+#include<stdio.h>
+
+void main()
+{  
+
+    int a = 25 , b = 15 , result ;
+   
+   result = a + b ;
+   
+   printf("sum of %d and %d is %d" , a, b, result );
+   
+   result = a - b ;
+   
+   printf("\n");
+   
+   printf( "difference of %d and %d is %d" , a ,b , result) ;
+   
+
+}
+
+
