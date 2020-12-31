3b7830bc3e41651e027a1a66b25cfd89a4c8d2d0
isha7337 <isha.bisht.2781@gmail.com>
2020-12-31 15:25:05 +0530

    My First Commit

---
+++ isha.c
@@ -0,0 +1,30 @@
+/* Code to demonstrate the working of arithematic opertator */
+
+#include<stdio.h>
+
+void main()
+{
+	int a = 25, b = 16, result;
+	
+	result = a + b;
+	printf("Sum of %d and %d is %d", a, b, result);
+	
+	printf("\n");
+	
+	result = a - b;
+	printf("Difference of %d and %d is %d", a, b, result);
+	
+	printf("\n");
+	
+	result = a * b;
+	printf("Multiplication of %d and %d is %d", a, b, result);
+	
+	printf("\n");
+	
+	result = a / b;
+	printf("Division of %d and %d is %d", a, b, result);
+	
+}
+
+
+
